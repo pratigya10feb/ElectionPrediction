907044ff78768273342dcb3d5f6dd260f2eecd79
pratigya agarwal <pratigya10feb@gmail.com>
2017-05-06 21:40:42 +0530

    Delete test_twitter_data.py

--- test_twitter_data.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import get_twitter_data
-
-## PLACE YOUR CREDENTIALS in config.json file or run this file with appropriate arguments from command line
-keyword = 'iphone'
-time = 'today'
-twitterData = get_twitter_data.TwitterData()
-tweets = twitterData.getTwitterData(keyword, time)
-print tweets
